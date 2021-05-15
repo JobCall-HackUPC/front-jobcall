6fc0bd69cd1642cddb2d8fa5cfc503e4c81835e6
Quim Picó Mora <quimpm99@gmail.com>
2021-05-15 09:57:09 +0200

    :grin:

---
+++ src/components/WebRTC.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <h2>1. Start your Webcam</h2>
+    <div class="videos">
+      <Webcam title="Local Stream" id="webcamVideo" :srcObject="localeSource" />
+      <Webcam
+        title="Remote Stream"
+        id="remoteVideo"
+        :srcObject="remoteSource"
+      />
+    </div>
+
+    <button @click="startWebcam">Start webcam</button>
+    <h2>2. Create a new Call</h2>
+    <button id="callButton" disabled>Create Call (offer)</button>
+
+    <h2>3. Join a Call</h2>
+    <p>Answer the call from a different browser window or device</p>
+
+    <input id="callInput" />
+    <button id="answerButton" disabled>Answer</button>
+
+    <h2>4. Hangup</h2>
+
+    <button id="hangupButton" disabled>Hangup</button>
+
+    <!-- <script type="module" src="../js/call.js"></script> -->
+  </div>
+</template>
+
+<script>
+import Webcam from "@/components/Webcam.vue";
+
+export default {
+  name: "WebRTC",
+  data() {
+    return {
+      servers: {
+        iceServers: [
+          {
+            urls: [
+              "stun:stun1.l.google.com:19302",
+              "stun:stun2.l.google.com:19302",
+            ],
+          },
+        ],
+        iceCandidatePoolSize: 10,
+      },
+      pc: null,
+      localeSource: null,
+      remoteSource: null,
+    };
+  },
+  components: {
+    Webcam,
+  },
+  methods: {
+    async startWebcam() {
+      var localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      var remoteStream = new MediaStream();
+      
+      localStream.getTracks().forEach((track) => {
+        this.pc.addTrack(track, localStream);
+      });
+
+      this.pc.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          remoteStream.addTrack(track);
+        });
+      };
+      this.localeSource = localStream;
+      this.remoteSource = remoteStream;
+
+      //this.props.localeSource = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    },
+  },
+  created() {
+    this.pc = new RTCPeerConnection(this.servers);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Webcam.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+        <span>
+            <h3>{{title}}</h3>
+            <video :id="id" autoplay playsinline></video>
+        </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WebRTC',
+  props: {
+    title: String,
+    id: String,
+    srcObject: Object,
+  },
+  data() {
+      return {
+        webcamVideo: null,      
+      };
+  },
+  updated() {
+      console.log("Updated " + this.title)
+    this.webcamVideo = document.getElementById(this.id);
+    this.webcamVideo.srcObject = this.srcObject;
+  }
+}
+</script>
+
+<style>
+
+</style>
